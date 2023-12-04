49d1e69594fffa5ea5aeba555862ab472a2dc9ec
hjenp22 <hjipark22@gmail.com>
2023-12-04 15:48:45 -0600

    added answers

---
+++ index.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+function generateREADME(answers) {
+    return `
+    # ${answers.title}
+    
+    ## Description
+    ${answers.description}
+    
+    ## Table of Contents
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [License](#license)
+    - [Contributing](#contributing)
+    - [Test](#tests)
+    - [Questions](#questions)
+    
+    ## Installation
+    ${answers.installation}
+    
+    ##Usage
+    `
+}
+
+inquirer
+    .prompt([
+
+    ])
