49dec6c7308b347f066c504b8637a1ffef37e2d3
hjenp22 <hjipark22@gmail.com>
2023-12-05 21:03:00 -0600

    prompt 1

--- index.js
+++ index.js
@@ -25,6 +25,9 @@ function generateREADME(answers) {
 
 inquirer
     .prompt([
-
-    
+        {
+            type: 'input',
+            name: 'title',
+            message: 'Enter Project Title'
+        },
     ])
