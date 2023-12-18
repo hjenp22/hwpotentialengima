a7393e775fa9928e50549d22094565ada9d0ab67
hjenp22 <hjipark22@gmail.com>
2023-12-18 09:52:40 -0800

    comment out to try again with user.Data

--- README.md
+++ README.md
@@ -1,33 +1,32 @@
 
-    # Generating a Professional README: Mod-9
+# READ ME GENERATOR
     
-    ## Description
-    Using knowledge of node.js to create this generator
+ ## Description
+ Make a readme using node and index.js
     
-    ## Table of Contents
-    - [Installation](#installation)
-    - [Usage](#usage)
-    - [License](#license)
-    - [Test](#tests)
-    - [Contributing](#contributing)
-    - [Questions](#questions)
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Test](#tests)
+- [Contributing](#contributing)
+- [Questions](#questions)
     
-    ## Installation
-    undefined
+## Installation
+undefined
     
-    ##Usage
-    This is a system that allows you to put input into the terminal to execute the generator
+##Usage
+me
 
-    ## License
-    ![License Badge](https://img.shields.io/badge/license-MIT-brightgreen)
+## License
+![License Badge](https://img.shields.io/badge/license-MIT-brightgreen)
 
-    ##Tests
-    Open terminal and enter 'npm i inquirer@8.2.4' followed by running the system by using 'node index.js' followed by answering the following prompts
+##Tests
+put in code, work on making functioning things 
 
-    ##Contributing
-    Contributing factors were myself, notes, and tutoring
+##Contributing
+me
 
-    ##Questions
-    For questions, reach me at hjipark22@gmail.com.
-    [Github Profile](https://github.com/hjenp22)
-    
+##Questions
+For questions, reach me at hjipark22@gmail.com.
+[Github Profile](https://github.com/hjenp22)
--- index.js
+++ index.js
@@ -1,91 +1,91 @@
-const { log } = require('console');
-const fs = require('fs');
-const inquirer = require('inquirer');
+// const { log } = require('console');
+// const fs = require('fs');
+// const inquirer = require('inquirer');
 
-function generateREADME(answers) {
-    return `
-# ${answers.title}
+// function generateREADME(answers) {
+//     return `
+// # ${answers.title}
     
- ## Description
- ${answers.description}
+//  ## Description
+//  ${answers.description}
     
-## Table of Contents
-- [Installation](#installation)
-- [Usage](#usage)
-- [License](#license)
-- [Test](#tests)
-- [Contributing](#contributing)
-- [Questions](#questions)
+// ## Table of Contents
+// - [Installation](#installation)
+// - [Usage](#usage)
+// - [License](#license)
+// - [Test](#tests)
+// - [Contributing](#contributing)
+// - [Questions](#questions)
     
-## Installation
-${answers.installation}
+// ## Installation
+// ${answers.installation}
     
-##Usage
-${answers.usage}
+// ##Usage
+// ${answers.usage}
 
-## License
-![License Badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)
+// ## License
+// ![License Badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)
 
-##Tests
-${answers.tests}
+// ##Tests
+// ${answers.tests}
 
-##Contributing
-${answers.contributing}
+// ##Contributing
+// ${answers.contributing}
 
-##Questions
-For questions, reach me at ${answers.email}.
-[Github Profile](https://github.com/${answers.username})
-`;
-}
+// ##Questions
+// For questions, reach me at ${answers.email}.
+// [Github Profile](https://github.com/${answers.username})
+// `;
+// }
 
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'title',
-            message: 'Enter project title'
-        },
-        {
-            type: 'input',
-            name: 'description',
-            message: 'Enter description'
-        },
-        {
-            type: 'input',
-            name: 'usage',
-            message: 'Enter usage'
-        },
-        {
-            type: 'list',
-            name: 'license',
-            nessage: 'Choose a license:',
-            choices: ['MIT', 'Apache', 'GPL', 'none'],
-        },
-        {
-            type: 'input',
-            name: 'tests',
-            message: 'Enter test instructions:',
-        },
-        {
-            type: 'input',
-            name: 'contributing',
-            message: 'Enter contributing factors:',
-        },
-        {
-            type: 'input',
-            name: 'username',
-            message: 'Enter Github username',
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'Enter email'
-        },
-    ])
-.then((answers)=> {
-    const READMEContent = generateREADME(answers);
-    fs.writeFile('README.md', READMEContent, (err)=> {
-        if (err) throw err;
-        console.log('README.MD created successfully!');
-    });
-});
+// inquirer
+//     .prompt([
+//         {
+//             type: 'input',
+//             name: 'title',
+//             message: 'Enter project title'
+//         },
+//         {
+//             type: 'input',
+//             name: 'description',
+//             message: 'Enter description'
+//         },
+//         {
+//             type: 'input',
+//             name: 'usage',
+//             message: 'Enter usage'
+//         },
+//         {
+//             type: 'list',
+//             name: 'license',
+//             nessage: 'Choose a license:',
+//             choices: ['MIT', 'Apache', 'GPL', 'none'],
+//         },
+//         {
+//             type: 'input',
+//             name: 'tests',
+//             message: 'Enter test instructions:',
+//         },
+//         {
+//             type: 'input',
+//             name: 'contributing',
+//             message: 'Enter contributing factors:',
+//         },
+//         {
+//             type: 'input',
+//             name: 'username',
+//             message: 'Enter Github username',
+//         },
+//         {
+//             type: 'input',
+//             name: 'email',
+//             message: 'Enter email'
+//         },
+//     ])
+// .then((answers)=> {
+//     const READMEContent = generateREADME(answers);
+//     fs.writeFile('README.md', READMEContent, (err)=> {
+//         if (err) throw err;
+//         console.log('README.MD created successfully!');
+//     });
+// });
